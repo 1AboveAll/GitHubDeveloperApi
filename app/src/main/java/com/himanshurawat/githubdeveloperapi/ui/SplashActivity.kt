7af3861b9a09a162ff87ce61f8e2ae27222d5c3f
Himanshu Rawat <rawath54@gmail.com>
2019-05-11 18:08:35 +0530

    Fin

--- app/src/main/java/com/himanshurawat/githubdeveloperapi/ui/SplashActivity.kt
+++ app/src/main/java/com/himanshurawat/githubdeveloperapi/ui/SplashActivity.kt
@@ -2,6 +2,7 @@ package com.himanshurawat.githubdeveloperapi.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.himanshurawat.githubdeveloperapi.R
@@ -17,8 +18,29 @@ class SplashActivity: AppCompatActivity() {
             R.layout.activity_splash
         )
 
-        startActivity(Intent(this@SplashActivity,
-            MainActivity::class.java))
+        var progressStatus = 0
+        val handler = Handler()
+
+
+        Thread(Runnable {
+            while (progressStatus < 100) {
+                progressStatus += 1
+                // Update the progress bar and display the
+                //current value in the text view
+                handler.post(Runnable {binding.activitySplashProgressBar.progress= progressStatus })
+                try {
+                    // Sleep for 50 milliseconds.
+                    Thread.sleep(20)
+                    if (progressStatus == 100) {
+                        startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+                        finish()
+                    }
+                } catch (e: Throwable) {
+                    e.printStackTrace()
+                }
+
+            }
+        }).start()
 
     }
 }
